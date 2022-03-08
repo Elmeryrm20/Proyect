e6b18bc617ab60ba863b1f74eed6b5bc18ddc2f9
Piero-21 <piero.sistemas21@gmail.com>
2022-03-08 18:25:28 -0500

    asd

--- PRESENTACION/FormAgregarMedicamento.cs
+++ PRESENTACION/FormAgregarMedicamento.cs
@@ -46,7 +46,7 @@ namespace PRESENTACION
         private void pictureBox2_Click(object sender, EventArgs e)
         {
 
-            pictureBox1.Image.Save(@"..\Resources\" + textNombre.Text + ".jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            pictureBox1.Image.Save(@"C:\Users\Piero\Source\Repos\Elmeryrm20\Proyect\PRESENTACION\Resources\" + textNombre.Text + ".jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
         }
     }
 }
