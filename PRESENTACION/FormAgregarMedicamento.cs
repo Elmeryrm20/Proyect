a7f893fbc3c9562d04d8f46f0cee0967b62fb232
Piero-21 <piero.sistemas21@gmail.com>
2022-03-08 16:06:40 -0500

    Mi cambio de hoy xd

--- PRESENTACION/FormAgregarMedicamento.cs
+++ PRESENTACION/FormAgregarMedicamento.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,26 @@ namespace PRESENTACION
         private void FormAgregarMedicamento_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.Title = "Escoja una imagen";
+            openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            openFileDialog1.ShowDialog();
+            
+
+            if (File.Exists(openFileDialog1.FileName))
+            {
+                string Imagen = openFileDialog1.FileName;
+                string nombre = openFileDialog1.SafeFileName;
+
+                this.Text = nombre;
+                pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Load(Imagen);
+            }
         }
     }
 }
