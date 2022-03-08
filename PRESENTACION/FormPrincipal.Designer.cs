5fb3405bda2d8a4b990e932f4e231ca675129063
ELMER YUNIOR RAMIREZ MELO <yunior20ramirez@gmail.com>
2022-03-07 23:15:04 -0500

    cambio remoto

--- PRESENTACION/FormPrincipal.Designer.cs
+++ PRESENTACION/FormPrincipal.Designer.cs
@@ -71,6 +71,7 @@ namespace PRESENTACION
             this.pictureBox1.Size = new System.Drawing.Size(27, 27);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // pictureBox2
             // 
@@ -89,6 +90,7 @@ namespace PRESENTACION
             this.pictureBox3.Size = new System.Drawing.Size(27, 27);
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
             // panel2
             // 
