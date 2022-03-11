8ac309411ac3a8f124a8a5891cf272b593849ef4
ELMER YUNIOR RAMIREZ MELO <yunior20ramirez@gmail.com>
2022-03-10 21:24:04 -0500

    cambio prosedencia

--- PRESENTACION/FormAgregarMedicamento.Designer.cs
+++ PRESENTACION/FormAgregarMedicamento.Designer.cs
@@ -38,7 +38,6 @@ namespace PRESENTACION
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.textCodigo = new System.Windows.Forms.TextBox();
@@ -49,7 +48,6 @@ namespace PRESENTACION
             this.cmbTipo = new System.Windows.Forms.ComboBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.textProcedencia = new System.Windows.Forms.TextBox();
             this.cmbCaja = new System.Windows.Forms.ComboBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
@@ -63,161 +61,135 @@ namespace PRESENTACION
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(16, 44);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(267, 36);
+            this.label1.Size = new System.Drawing.Size(218, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Agregar Producto";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(123, 103);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(92, 84);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.Size = new System.Drawing.Size(40, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Código";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(117, 137);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(88, 111);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.Size = new System.Drawing.Size(44, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Nombre";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(111, 170);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(83, 138);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 17);
+            this.label4.Size = new System.Drawing.Size(49, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "Cantidad";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(96, 202);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(72, 164);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(81, 17);
+            this.label5.Size = new System.Drawing.Size(60, 13);
             this.label5.TabIndex = 4;
             this.label5.Text = "Laboratorio";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(27, 234);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(20, 190);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(146, 17);
+            this.label6.Size = new System.Drawing.Size(112, 13);
             this.label6.TabIndex = 5;
             this.label6.Text = "Fecha de vencimiento";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(115, 268);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(86, 218);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(62, 17);
+            this.label7.Size = new System.Drawing.Size(46, 13);
             this.label7.TabIndex = 6;
             this.label7.Text = "Gramaje";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(139, 303);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(104, 246);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(36, 17);
+            this.label8.Size = new System.Drawing.Size(28, 13);
             this.label8.TabIndex = 7;
             this.label8.Text = "Tipo";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(136, 334);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Location = new System.Drawing.Point(102, 271);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(38, 17);
+            this.label9.Size = new System.Drawing.Size(29, 13);
             this.label9.TabIndex = 8;
             this.label9.Text = "Para";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(87, 366);
-            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(87, 17);
-            this.label10.TabIndex = 9;
-            this.label10.Text = "Procedencia";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(139, 399);
-            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Location = new System.Drawing.Point(104, 296);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(36, 17);
+            this.label11.Size = new System.Drawing.Size(28, 13);
             this.label11.TabIndex = 10;
             this.label11.Text = "Caja";
             // 
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(563, 299);
-            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Location = new System.Drawing.Point(422, 243);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(119, 17);
+            this.label12.Size = new System.Drawing.Size(90, 13);
             this.label12.TabIndex = 13;
             this.label12.Text = "Suba una imagen";
             // 
             // textCodigo
             // 
-            this.textCodigo.Location = new System.Drawing.Point(184, 100);
-            this.textCodigo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textCodigo.Location = new System.Drawing.Point(138, 81);
             this.textCodigo.Name = "textCodigo";
-            this.textCodigo.Size = new System.Drawing.Size(257, 22);
+            this.textCodigo.Size = new System.Drawing.Size(194, 20);
             this.textCodigo.TabIndex = 14;
             // 
             // textNombre
             // 
-            this.textNombre.Location = new System.Drawing.Point(184, 133);
-            this.textNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textNombre.Location = new System.Drawing.Point(138, 108);
             this.textNombre.Name = "textNombre";
-            this.textNombre.Size = new System.Drawing.Size(257, 22);
+            this.textNombre.Size = new System.Drawing.Size(194, 20);
             this.textNombre.TabIndex = 15;
             // 
             // textCantidad
             // 
-            this.textCantidad.Location = new System.Drawing.Point(184, 165);
-            this.textCantidad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textCantidad.Location = new System.Drawing.Point(138, 134);
             this.textCantidad.Name = "textCantidad";
-            this.textCantidad.Size = new System.Drawing.Size(257, 22);
+            this.textCantidad.Size = new System.Drawing.Size(194, 20);
             this.textCantidad.TabIndex = 16;
             // 
             // textLaboratorio
             // 
-            this.textLaboratorio.Location = new System.Drawing.Point(184, 197);
-            this.textLaboratorio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textLaboratorio.Location = new System.Drawing.Point(138, 160);
             this.textLaboratorio.Name = "textLaboratorio";
-            this.textLaboratorio.Size = new System.Drawing.Size(257, 22);
+            this.textLaboratorio.Size = new System.Drawing.Size(194, 20);
             this.textLaboratorio.TabIndex = 17;
             // 
             // textGramaje
             // 
-            this.textGramaje.Location = new System.Drawing.Point(184, 265);
-            this.textGramaje.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textGramaje.Location = new System.Drawing.Point(138, 215);
             this.textGramaje.Name = "textGramaje";
-            this.textGramaje.Size = new System.Drawing.Size(257, 22);
+            this.textGramaje.Size = new System.Drawing.Size(194, 20);
             this.textGramaje.TabIndex = 19;
             // 
             // cmbTipo
@@ -225,19 +197,17 @@ namespace PRESENTACION
             this.cmbTipo.FormattingEnabled = true;
             this.cmbTipo.Items.AddRange(new object[] {
             "xdd"});
-            this.cmbTipo.Location = new System.Drawing.Point(184, 299);
-            this.cmbTipo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbTipo.Location = new System.Drawing.Point(138, 243);
             this.cmbTipo.Name = "cmbTipo";
-            this.cmbTipo.Size = new System.Drawing.Size(257, 24);
+            this.cmbTipo.Size = new System.Drawing.Size(194, 21);
             this.cmbTipo.TabIndex = 20;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(183, 331);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton1.Location = new System.Drawing.Point(137, 269);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(69, 21);
+            this.radioButton1.Size = new System.Drawing.Size(55, 17);
             this.radioButton1.TabIndex = 21;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Adulto";
@@ -246,32 +216,22 @@ namespace PRESENTACION
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(264, 332);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton2.Location = new System.Drawing.Point(198, 270);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(92, 21);
+            this.radioButton2.Size = new System.Drawing.Size(72, 17);
             this.radioButton2.TabIndex = 22;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Pediátrico";
             this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // textProcedencia
-            // 
-            this.textProcedencia.Location = new System.Drawing.Point(184, 362);
-            this.textProcedencia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textProcedencia.Name = "textProcedencia";
-            this.textProcedencia.Size = new System.Drawing.Size(257, 22);
-            this.textProcedencia.TabIndex = 23;
             // 
             // cmbCaja
             // 
             this.cmbCaja.FormattingEnabled = true;
             this.cmbCaja.Items.AddRange(new object[] {
             "xdd"});
-            this.cmbCaja.Location = new System.Drawing.Point(183, 394);
-            this.cmbCaja.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbCaja.Location = new System.Drawing.Point(137, 292);
             this.cmbCaja.Name = "cmbCaja";
-            this.cmbCaja.Size = new System.Drawing.Size(259, 24);
+            this.cmbCaja.Size = new System.Drawing.Size(195, 21);
             this.cmbCaja.TabIndex = 24;
             // 
             // openFileDialog1
@@ -281,10 +241,9 @@ namespace PRESENTACION
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.pictureBox2.Location = new System.Drawing.Point(521, 384);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Location = new System.Drawing.Point(391, 312);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(200, 50);
+            this.pictureBox2.Size = new System.Drawing.Size(150, 41);
             this.pictureBox2.TabIndex = 12;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
@@ -292,33 +251,30 @@ namespace PRESENTACION
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.pictureBox1.Location = new System.Drawing.Point(521, 103);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(391, 84);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 192);
+            this.pictureBox1.Size = new System.Drawing.Size(150, 156);
             this.pictureBox1.TabIndex = 11;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // dtFecha_Vencimiento
             // 
-            this.dtFecha_Vencimiento.Location = new System.Drawing.Point(183, 229);
-            this.dtFecha_Vencimiento.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtFecha_Vencimiento.Location = new System.Drawing.Point(137, 186);
             this.dtFecha_Vencimiento.MinDate = new System.DateTime(2022, 3, 9, 0, 7, 53, 0);
             this.dtFecha_Vencimiento.Name = "dtFecha_Vencimiento";
-            this.dtFecha_Vencimiento.Size = new System.Drawing.Size(259, 22);
+            this.dtFecha_Vencimiento.Size = new System.Drawing.Size(195, 20);
             this.dtFecha_Vencimiento.TabIndex = 25;
             this.dtFecha_Vencimiento.Value = new System.DateTime(2022, 3, 9, 0, 7, 53, 0);
             this.dtFecha_Vencimiento.ValueChanged += new System.EventHandler(this.dtFecha_Vencimiento_ValueChanged);
             // 
             // FormAgregarMedicamento
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(789, 500);
+            this.ClientSize = new System.Drawing.Size(592, 406);
             this.Controls.Add(this.dtFecha_Vencimiento);
             this.Controls.Add(this.cmbCaja);
-            this.Controls.Add(this.textProcedencia);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.cmbTipo);
@@ -331,7 +287,6 @@ namespace PRESENTACION
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label11);
-            this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -342,7 +297,6 @@ namespace PRESENTACION
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormAgregarMedicamento";
             this.Text = "FormAgregarMedicamento";
             this.Load += new System.EventHandler(this.FormAgregarMedicamento_Load);
@@ -364,7 +318,6 @@ namespace PRESENTACION
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
@@ -377,7 +330,6 @@ namespace PRESENTACION
         private System.Windows.Forms.ComboBox cmbTipo;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.TextBox textProcedencia;
         private System.Windows.Forms.ComboBox cmbCaja;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.DateTimePicker dtFecha_Vencimiento;
