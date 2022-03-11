87f117124204453354325bba2ed1473e7bce037c
ELMER YUNIOR RAMIREZ MELO <yunior20ramirez@gmail.com>
2022-03-10 23:48:13 -0500

    cambio conexion

---
+++ DATOS/Consultas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATOS
+{
+    internal class Consultas
+    {
+        D_Empleado Conexion= new D_Empleado();
+    }
+}
