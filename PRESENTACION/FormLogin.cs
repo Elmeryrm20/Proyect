1b45ab3eeee263dcc5fd6abc696f6f3078ecaff7
Piero-21 <piero.sistemas21@gmail.com>
2022-03-10 22:01:23 -0500

    xdd

--- PRESENTACION/FormLogin.cs
+++ PRESENTACION/FormLogin.cs
@@ -17,41 +17,7 @@ namespace PRESENTACION
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
 
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-            
-            if (radioButton1.Checked==true)
-            {
-                textBox1.MaxLength = 8;
-                textBox1.Text = textBox1.Text.Trim();
-            }
-            else if (radioButton2.Checked==true)
-            {
-                label2.Text = "PASAPORTE :";
-            }
-            else
-            {
-                label2.Text = "DNI :";
-            }
-
-            label2.Text = "DNI :";
-
-        }
 
         private void pictureBox3_Click(object sender, EventArgs e)
         {
@@ -63,49 +29,12 @@ namespace PRESENTACION
             this.WindowState = FormWindowState.Minimized;
         }
 
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
         {
             if (radioButton1.Checked==true)
             {
                 validar.soloNumeros(e);
-            }
-            
-            
-        }
-
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-
+            }  
         }
 
         private void button1_Click_1(object sender, EventArgs e)
@@ -122,11 +51,6 @@ namespace PRESENTACION
                 label2.Text = "PASAPORTE";
                 textBox1.MaxLength = 20;
             }
-            
-        }
-
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
             
         }
     }
