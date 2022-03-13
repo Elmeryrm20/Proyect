afb4653436afc606f5c228b0478a02b3c136bbb7
ELMER YUNIOR RAMIREZ MELO <yunior20ramirez@gmail.com>
2022-03-13 00:49:12 -0500

    Cambios Agregar_Lab

--- PRESENTACION/FormAgregarLaboratorio.cs
+++ PRESENTACION/FormAgregarLaboratorio.cs
@@ -16,5 +16,30 @@ namespace PRESENTACION
         {
             InitializeComponent();
         }
+
+        private void FormAgregarLaboratorio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
