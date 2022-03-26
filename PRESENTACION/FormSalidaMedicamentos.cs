2ea2ad2181864f6e2c160c4b4a2c73ef73f9f593
ZttaR quispe oropeza <ef_jhean@hotmail.com>
2022-03-25 23:37:05 -0500

    mas cambios

--- PRESENTACION/FormSalidaMedicamentos.cs
+++ PRESENTACION/FormSalidaMedicamentos.cs
@@ -1,7 +1,5 @@
-﻿using System.Data;
-using DATOS;
+﻿using DATOS;
 using System.Windows.Forms;
-using System.IO;
 
 namespace PRESENTACION
 {
@@ -25,15 +23,15 @@ namespace PRESENTACION
         Consultas consultas = new Consultas();
 
 
-        public void AgregarMedicamentoDgv(string Med_Codigo, int cantidad)
-        {
-            //Adicionamos nuevo renglon
-            int n = DgvSalida.Rows.Add();
+        //public void AgregarMedicamentoDgv(string Med_Codigo, int cantidad)
+        //{
+        //    //Adicionamos nuevo renglon
+        //    int n = DgvSalida.Rows.Add();
 
-            //Colocamos la información
-            DgvSalida.Rows[n].Cells[0].Value = Med_Codigo;
-            DgvSalida.Rows[n].Cells[1].Value = cantidad.ToString();
-        }
+        //    //Colocamos la información
+        //    DgvSalida.Rows[n].Cells[0].Value = Med_Codigo;
+        //    DgvSalida.Rows[n].Cells[1].Value = cantidad.ToString();
+        //}
 
         private void PibAgregarMed_Click(object sender, System.EventArgs e)
         {
@@ -52,17 +50,30 @@ namespace PRESENTACION
 
         private void PibConfirmar_Click(object sender, System.EventArgs e)
         {
+            //    if (DgvSalida.Rows.Count >= 0)
+            //    {
+            //        for (int i = 0; i < DgvSalida.Rows.Count; i++)
+            //        {
+            //            consultas.D_ActualizarEgreso((int)DgvSalida.Rows[i].Cells[0].Value, (int)DgvSalida.Rows[i].Cells[0].Value);
+            //            //codigo
+            //            //nombre
+            //            //cantidad
 
-            //    //consultas.SP_Agregar_Egreso_Medicamento(Med_Codigo, cantidad);
-            //    consultas.D_ActualizarEgreso(Med_Codigo, cantidad);
 
-            //    consultas.SP_Agregar_Detalle_Egreso(Med_Codigo, cantidad, DateTime.Now.ToString("yyyy-MM-dd") + " " + DateTime.Now.ToString("HH-mm-ss"), DNI, 4);
-            //    MessageBox.Show("Actualización Exitosa");
+            //            consultas.SP_Agregar_Detalle_Egreso(Med_Codigo, cantidad, DateTime.Now.ToString("yyyy-MM-dd") + " " + DateTime.Now.ToString("HH-mm-ss"), DNI, 4);
 
-            //    FormMedicamentos FrmMed = Owner as FormMedicamentos;
-            //    FrmMed.Rellenartabla();
-            //    FrmMed.dgb_Medicamentos.CurrentCell = FrmMed.dgb_Medicamentos.Rows[Med_Codigo - 1].Cells[0];
-            //    Close();
+            //        }
+
+            //        //    MessageBox.Show("Actualización Exitosa");
+
+            //        //    FormMedicamentos FrmMed = Owner as FormMedicamentos;
+            //        //    FrmMed.Rellenartabla();
+            //        //    FrmMed.dgb_Medicamentos.CurrentCell = FrmMed.dgb_Medicamentos.Rows[Med_Codigo - 1].Cells[0];
+            //        //    Close();
+            //    }
+
+            MessageBox.Show("Por el momento no están habilitados los egresos.");
+
         }
 
         private void FormSalidaMedicamentos_Load(object sender, System.EventArgs e)
@@ -75,6 +86,9 @@ namespace PRESENTACION
             CmbEncargado.DisplayMember = "Trabajador";
             CmbEncargado.DataSource = consultas.D_MostrarColaboradores(DNI);
             //CmbEncargado.ValueMember = "Encargado_Desc";
+
+            CmbTipoDNI.DisplayMember = "Tid_Descripcion";
+            CmbTipoDNI.DataSource = consultas.D_TipoDNI();
 
         }
     }
