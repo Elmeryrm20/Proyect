3c60101fa21bdc3d428e48a17ec5c90576ce399d
ZttaR quispe oropeza <ef_jhean@hotmail.com>
2022-05-06 01:28:19 -0500

    Entrada Medicamento Completado

--- PRESENTACION/FormIngreso.Designer.cs
+++ PRESENTACION/FormIngreso.Designer.cs
@@ -29,46 +29,31 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnSerrar = new System.Windows.Forms.PictureBox();
             this.lbl_Nombre = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.Txt_Cantidad = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.dtp_FechaVencimiento = new PRESENTACION.SelectorDatePicker();
-            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnSerrar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(53)))), ((int)(((byte)(46)))));
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.btnSerrar);
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(348, 37);
+            this.panel1.Size = new System.Drawing.Size(420, 27);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "INGRESO";
             // 
             // btnSerrar
             // 
             this.btnSerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnSerrar.BackColor = System.Drawing.Color.Transparent;
             this.btnSerrar.Image = global::PRESENTACION.Properties.Resources.BotonVentanaCerrar01;
-            this.btnSerrar.Location = new System.Drawing.Point(318, 3);
+            this.btnSerrar.Location = new System.Drawing.Point(305, 276);
             this.btnSerrar.Name = "btnSerrar";
             this.btnSerrar.Size = new System.Drawing.Size(24, 24);
             this.btnSerrar.TabIndex = 3;
@@ -77,45 +62,33 @@
             // 
             // lbl_Nombre
             // 
-            this.lbl_Nombre.AutoSize = true;
-            this.lbl_Nombre.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Nombre.Location = new System.Drawing.Point(43, 56);
+            this.lbl_Nombre.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nombre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(149)))), ((int)(((byte)(178)))));
+            this.lbl_Nombre.Location = new System.Drawing.Point(145, 30);
             this.lbl_Nombre.Name = "lbl_Nombre";
-            this.lbl_Nombre.Size = new System.Drawing.Size(64, 18);
+            this.lbl_Nombre.Size = new System.Drawing.Size(263, 37);
             this.lbl_Nombre.TabIndex = 1;
             this.lbl_Nombre.Text = "Nombre";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 120);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Cantidad Ingreso:";
+            this.lbl_Nombre.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Txt_Cantidad
             // 
-            this.Txt_Cantidad.Location = new System.Drawing.Point(150, 120);
+            this.Txt_Cantidad.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Txt_Cantidad.Font = new System.Drawing.Font("Century Gothic", 30.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Cantidad.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(177)))), ((int)(((byte)(101)))));
+            this.Txt_Cantidad.Location = new System.Drawing.Point(230, 94);
             this.Txt_Cantidad.Name = "Txt_Cantidad";
-            this.Txt_Cantidad.Size = new System.Drawing.Size(158, 20);
+            this.Txt_Cantidad.Size = new System.Drawing.Size(99, 51);
             this.Txt_Cantidad.TabIndex = 4;
+            this.Txt_Cantidad.Text = "555";
+            this.Txt_Cantidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.Txt_Cantidad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txt_Cantidad_KeyPress);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(58, 159);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "F.Vencimiento:";
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::PRESENTACION.Properties.Resources.BotonFormConfirmar01;
-            this.pictureBox1.Location = new System.Drawing.Point(130, 228);
+            this.pictureBox1.Location = new System.Drawing.Point(161, 268);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(110, 40);
             this.pictureBox1.TabIndex = 3;
@@ -126,33 +99,34 @@
             // 
             this.dtp_FechaVencimiento.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(181)))), ((int)(((byte)(230)))));
             this.dtp_FechaVencimiento.BorderSize = 0;
+            this.dtp_FechaVencimiento.CalendarFont = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtp_FechaVencimiento.CalendarForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(141)))), ((int)(((byte)(216)))));
             this.dtp_FechaVencimiento.CalendarMonthBackground = System.Drawing.Color.White;
             this.dtp_FechaVencimiento.CalendarTitleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(181)))), ((int)(((byte)(230)))));
             this.dtp_FechaVencimiento.CalendarTitleForeColor = System.Drawing.Color.White;
-            this.dtp_FechaVencimiento.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtp_FechaVencimiento.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtp_FechaVencimiento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtp_FechaVencimiento.Location = new System.Drawing.Point(150, 159);
+            this.dtp_FechaVencimiento.Location = new System.Drawing.Point(214, 189);
             this.dtp_FechaVencimiento.MaxDate = new System.DateTime(2050, 12, 31, 0, 0, 0, 0);
             this.dtp_FechaVencimiento.MinDate = new System.DateTime(2022, 4, 4, 0, 0, 0, 0);
-            this.dtp_FechaVencimiento.MinimumSize = new System.Drawing.Size(0, 20);
+            this.dtp_FechaVencimiento.MinimumSize = new System.Drawing.Size(4, 20);
             this.dtp_FechaVencimiento.Name = "dtp_FechaVencimiento";
-            this.dtp_FechaVencimiento.Size = new System.Drawing.Size(147, 23);
+            this.dtp_FechaVencimiento.Size = new System.Drawing.Size(127, 27);
             this.dtp_FechaVencimiento.SkinColor = System.Drawing.Color.White;
             this.dtp_FechaVencimiento.TabIndex = 63;
-            this.dtp_FechaVencimiento.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(141)))), ((int)(((byte)(216)))));
+            this.dtp_FechaVencimiento.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
             this.dtp_FechaVencimiento.Value = new System.DateTime(2022, 4, 4, 1, 35, 32, 0);
             // 
             // FormIngreso
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(348, 304);
-            this.Controls.Add(this.dtp_FechaVencimiento);
-            this.Controls.Add(this.label3);
+            this.BackgroundImage = global::PRESENTACION.Properties.Resources.FondoEntrada01;
+            this.ClientSize = new System.Drawing.Size(420, 320);
+            this.Controls.Add(this.btnSerrar);
             this.Controls.Add(this.Txt_Cantidad);
+            this.Controls.Add(this.dtp_FechaVencimiento);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.lbl_Nombre);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -160,8 +134,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormIngreso";
             this.Load += new System.EventHandler(this.FormIngreso_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnSerrar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -174,11 +146,8 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox btnSerrar;
         private System.Windows.Forms.Label lbl_Nombre;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox Txt_Cantidad;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label3;
         private SelectorDatePicker dtp_FechaVencimiento;
     }
 }
